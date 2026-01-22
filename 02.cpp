71ba53da0d3b208b85844abfa3e6f2bfc8afb072
Amruta <amrutavarma227@gmail.com>
2026-01-23 02:00:12 +0530

    Resolve merge conflict

---
+++ 02.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+//#include<vector>
+using namespace std;
+
+int main() {
+    /*std::vector<int> numbers = {1, 2, 3, 4, 5};
+    int sum = 0;
+
+    for (const auto& num : numbers) {
+        sum += num;
+    }
+
+    std::cout << "The sum of the numbers is: " << sum << std::endl;
+    */
+   int a = 5;
+   int b = 10;
+    cout << "The sum of a and b is: " << a + b << endl;
+    return 0;
+}
